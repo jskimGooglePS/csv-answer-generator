6f847b331ff925927179c1c584f63d6b9be4a5b6
root <root@junsu-kim.localdomain>
2026-01-08 15:56:40 +0900

    20260108: 초기 csv 답변 생성 코드 추가

--- README.md
+++ README.md
@@ -1,2 +1,36 @@
 # csv-answer-generator
 generate_csv_answer_automation
+
+## 환경 정보
+```
+pip install pandas
+pip install tqdm
+pip install google-genai
+
+pip freeze > requirements.txt
+```
+
+## 환경 세팅
+```bash
+cd csv-answer-generator
+python3 -m venv .venv
+source .venv/bin/activate
+
+pip install -r requirements.txt
+```
+### 파일 실행
+
+```bash
+python3 -m make_csv_results
+```
+
+## 실행 방법
+
+### input 데이터 파일 위치
+data/input/
+
+### output 데이터 파일 위치
+data/output/
+
+
+
---
+++ make_csv_results.py
@@ -0,0 +1,77 @@
+import pandas as pd
+from tqdm.auto import tqdm 
+
+from google.genai import Client
+from google.genai import types
+
+import asyncio
+
+#모델 및 데이터 선택
+model = "gemini-2.5-flash"
+df = pd.read_csv("./data/input/Planning_테스트 자동화_text.csv")
+
+client = Client(
+    vertexai=True,
+    project="beha-data",
+    location="us-central1",
+)
+
+# gemini 실행 함수
+# def generate_answer(model, content):
+    
+#     # 2. 텍스트 생성
+#     response = client.models.generate_content(
+#         model=model,
+#         contents=str(content)
+#     )
+    
+#     return response.text
+
+# gemini 실행 루프
+# for index, row in tqdm(df.iterrows()):
+#     query = row.get("사용자 질의")
+#     template = row.get("프롬프트")
+#     prompt = template.replace("{사용자 질의}", str(query))
+#     answer = generate_answer(model, prompt)
+#     df.loc[index, '결과'] = answer
+
+# gemini 병렬 실행 함수
+async def generate_batch_answers(model, prompts):
+    async def call_api(contents):
+        try:
+            # .aio는 비동기 클라이언트를 호출합니다.
+            response = await client.aio.models.generate_content(model=model, contents=contents)
+            return response.text
+        except Exception as e:
+            return f"Error: {e}"
+    tasks = [call_api(p) for p in prompts]
+
+    return await asyncio.gather(*tasks)
+   
+async def process_all():
+
+    batch_size = 8
+    results = []
+
+    # gemini 병렬 실행 루프
+    for i in tqdm(range(0, len(df), batch_size)):
+        batch_df = df.iloc[i : i + batch_size]
+
+        prompts = []
+        for _, row in batch_df.iterrows():
+            query = row.get("사용자 질의")
+            template = row.get("프롬프트")
+            prompt = template.replace("{사용자 질의}", str(query))
+            prompts.append(prompt)
+
+        batch_answers = await generate_batch_answers(model, prompts)
+        results.extend(batch_answers)
+
+    df['결과'] = results
+
+    df.to_csv("./data/output/result_csv.csv", index=False)
+
+    print("CSV 생성 완료!")
+
+if __name__ == "__main__":
+    asyncio.run(process_all())
